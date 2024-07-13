716c1f653977ca1dcf34d7cbaf59cc22a1ed6f7a
SAINT FOURIER ONESYME <saintfourieronesyme@gmail.com>
2024-07-13 11:14:29 +0000

    -  creation du dossier templates et static de user 	-  retirer la version de django, de sorte a installer la version resente disponible

--- app_edcp/user/views.py
+++ app_edcp/user/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 # Create your views here.
 
 # user/views.py
 def index(request):
-    return HttpResponse("Bienvenue sur la page d'accueil de l'application Utilisateurs")
+    """ Vue index user """
+    return render(request, 'user/index.html')
