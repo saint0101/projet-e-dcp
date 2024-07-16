9622ea0c7312df1dc253494fb56bc8d8f1ff8d0a
James Millenium <millenium.walka@yahoo.com>
2024-07-16 14:46:07 +0000

    Mise en place du squelette du site, intégration de bootstrap. Création des applications 'Public' et 'Dashboard'. Urls et design en cours.

---
+++ app_edcp/public/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'public'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('a-propos/', views.a_propos, name='a_propos'),
+    path('guides/', views.guides, name='guides'),
+    path('faq/', views.faq, name='faq'),
+    path('contacts/', views.contacts, name='contacts'),
+    # autres motifs d'URL
+]
