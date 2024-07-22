a68921a19df93826a5fae25dd57ae2620411376f
James Millenium <millenium.walka@yahoo.com>
2024-07-22 19:12:05 +0000

    Gestion du menu OK

--- app_edcp/dashboard/context_processors.py
+++ app_edcp/dashboard/context_processors.py
@@ -1,7 +1,3 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
-from django.contrib.auth.decorators import login_required
-
 # Create your views here.
 """
 Menu à afficher dans barre latérale (sidebar.html) du tableau de bord pour les utilisateurs de type client.
@@ -306,29 +302,17 @@ MENU_MGR = [
 ]
 
 
-# def get_menu(req)
 
-
-@login_required(login_url=reverse_lazy('login'))
-def index(request):
+def get_menu(request):
   """
-  Vue qui génère la page de tableau de bord d'un utilisateur
-  Accepte en paramètre la requête HTTP (objet request).
-  Renvoie la page de tableau de bord avec le contexte de menu correspondant à l'utilisateur.
+  Returns the menu for the given request.
+  Parameters:
+    request (HttpRequest): The HTTP request object.
+  Returns:
+    list: The menu for the user.
   """
   user = request.user
-  if user.is_staff :
-    return render(request, 'dashboard/index.html', context={'menu': MENU_MGR})
+  if user.is_authenticated and user.is_staff :
+    return {'get_menu': MENU_MGR} 
   
-  return render(request, 'dashboard/index.html', context={'menu': MENU_CLIENT})
-
-
-""" def index_client(request):
-  return render(request, 'dashboard/client/index_c.html', context={'menu': MENU_CLIENT})
-
-
-def index_mgr(request):
-  if not request.user.is_authenticated :
-    return redirect('dashboard:index_client')
-  
-  return render(request, 'dashboard/manager/index_m.html') """
+  return {'get_menu': MENU_CLIENT} 
