ff5a15ae6e58b409cf2148181d629b963b6e73fc
James Millenium <millenium.walka@yahoo.com>
2024-07-22 17:57:39 +0000

    début enregistrement

--- app_edcp/dashboard/urls.py
+++ app_edcp/dashboard/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'dashboard'
 
 urlpatterns = [
-    path('', views.index, name='index'), # url du tableau de bord
-    # path('client/', views.index_client, name='index_client'),
-    # path('manager/', views.index_mgr, name='index_mgr'),
+    path('', views.index, name='index'),
+    path('enregistrement/', include('enregistrement.urls')),
 ]
