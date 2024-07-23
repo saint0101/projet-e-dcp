4bd93a1c17b949ef1480a21a2d539e860ba3fb21
SAINT FOURIER ONESYME <saintfourieronesyme@gmail.com>
2024-07-23 18:55:40 +0000

    - ajout du module d'envoie de mail au  module enregistrement

--- app_edcp/app_edcp/settings.py
+++ app_edcp/app_edcp/settings.py
@@ -216,4 +216,5 @@ EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'info-apdcp@artci.ci')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'jeFs3oq-jycjZr%1sYwfu')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'info-apdcp@artci.ci')
 
