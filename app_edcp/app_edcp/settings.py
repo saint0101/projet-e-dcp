98ad038d6b1f77c5811ff594cfedf9255b896b34
James Millenium <millenium.walka@yahoo.com>
2024-08-02 11:55:56 +0000

    Corrections setiings.py, Toastr et index dashboard

--- app_edcp/app_edcp/settings.py
+++ app_edcp/app_edcp/settings.py
@@ -127,7 +127,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
-""" AUTH_PASSWORD_VALIDATORS = [
+AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
@@ -140,7 +140,7 @@ DATABASES = {
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
-] """
+]
 
 
 # Internationalization
